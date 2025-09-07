d6a78836e3f5a6ca64088db0f016ce6030d6f9b8
qezlou <mahdiqezlou@gmail.com>
2025-09-07 13:18:45 -0500

    run on Ls6

--- scripts/get_cov_hub.py
+++ scripts/get_cov_hub.py
@@ -1,11 +1,17 @@
 # I cannot pip install on the Hub
 import sys
 import os
-sys.path.append(os.path.abspath('/home/jovyan/work/hetdex/packs/private-het-data/src/het_cov'))
 
-import fibers
+# Determine if we are on the Hub by checking for a specific environment variable
+if 'JUPYTERHUB_USER' in os.environ:
+    sys.path.append(os.path.abspath('/home/jovyan/work/hetdex/packs/private-het-data/src/het_cov'))
+    import fibers
+    data_dir = '/home/jovyan/work/hetdex/data/'
+else:
+    import het_cov import fibers
+    data_dir = '/work/06536/qezlou/hetdex/data/'
 
-data_dir = '/home/jovyan/work/hetdex/data/'
+
 fibs = fibers.Fibers(data_dir)
 
 fibs.get_cov()
